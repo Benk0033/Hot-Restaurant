df496e2dccf7bab898b0cdf0e49cfd608955404f
nance <48306129+NanceCA@users.noreply.github.com>
2019-06-15 10:10:38 -0700

    fixing the api routes

--- jsonRoutes.js
+++ jsonRoutes.js
@@ -1,15 +1,25 @@
 //this should probably be modularized?
 var require = ("express");
 
+//can we import the reservations?
+
 var app = express();
 
-app.post("./viewTables", function (req, res) {
 
-    if (tables < 5) {
-        //allow the table to be posted
+app.post("./api/viewTables", function (req, res) {
+
+    req.json(reservations)
+    //do I need to store this in req.body?
+    var newReservation = req.body;
+
+    if (tables.length < 5) {
+        //allow the table to be posted if there are less than 5
+        tables.push(newReservation);
+        res.json(newReservation)
     }
     else {
-        //
+        waitlist.push(newReservation);
+        res.json(waitlist);
     }
 
 });
